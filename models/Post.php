8f6d05f4a467a0097e8d957f325051a4fc008682
Leweyse <darylcastro67@gmail.com>
2021-11-03 16:28:04 +0100

    added logic to classes

--- models/Post.php
+++ models/Post.php
@@ -3,5 +3,68 @@ declare(strict_types=1);
 
 class Post
 {
+    private string $title = "title";
+    private string $date = "date";
+    private string $content = "content";
+    private string $author = "author";
+
+    private array $data = [];
+    private array $fields = [];
+    private array $errors = [];
+
+    private bool $validated = false;
+
+    public function __construct() {
+        $this -> fields = [$this -> title, $this -> date, $this -> content, $this -> author];
+
+        foreach ($this -> fields as $field) {
+            if ($field == "date") array_push($this -> data, date("F j, Y, g:i a"));
+            else array_push($this -> data, test_input($_POST["$field"]));
+        }
+    }
+
+    public function validateFields() : void
+    {
+        foreach ($this -> fields as $key => $field) {
+            if ($this -> data[$key] == "") array_push($this -> errors, ucfirst("$field is required!" . "<br>"));
+        }
+
+        if (count($this -> errors) > 0) {
+            $this -> validated = false;
+            foreach ($this -> errors as $error) errorComponent($error);
+        } else {
+            $this -> validated = true;
+            successComponent("Added message!");
+        }
+    }
+
+    public function getValidation() : bool
+    {
+        return $this -> validated;
+    }
     
+    public function getFields() : array
+    {
+        return $this -> fields;
+    }
+
+    public function getTitle() : string
+    {
+        return $this -> data[0];
+    }
+
+    public function getDate() : string
+    {
+        return $this -> data[1];
+    }
+
+    public function getContent() : string
+    {
+        return $this -> data[2];
+    }
+
+    public function getAuthor() : string
+    {
+        return $this -> data[3];
+    }
 }
--- models/PostLoader.php
+++ models/PostLoader.php
@@ -3,5 +3,51 @@ declare(strict_types=1);
 
 class PostLoader
 {
+    private $post;
+    private $fields;
 
+    private $data = [];
+    private $oldData = [];
+
+    private const MAX_MSG = 20;
+
+    public function __construct($post) {
+        $this -> post = $post;
+        $this -> fields = $post -> getFields();
+
+        $this -> data = Array (
+            $this -> fields[0] => $this -> post -> getTitle(),
+            $this -> fields[1] => $this -> post -> getDate(),
+            $this -> fields[2] => $this -> post -> getContent(),
+            $this -> fields[3] => $this -> post -> getAuthor()
+        );
+    }
+
+    public function setDecoded()
+    {
+        $jsonData = file_get_contents("data.json");
+        $decode = json_decode($jsonData, true);
+
+        if (isset($decode['data'])) {
+            $this -> oldData = $decode['data'];
+            array_unshift($this -> oldData, $this -> data);
+        } else $this -> oldData = [0 => $this -> data];
+    }
+
+    public function setEncoded()
+    {
+        $this -> setDecoded();
+
+        if (count($this -> oldData) > self::MAX_MSG) {
+            array_pop($this->oldData);
+            $json = json_encode(array("data" => $this -> oldData));
+        } else $json = json_encode(array("data" => $this -> oldData));
+
+        file_put_contents("data.json", $json);
+    }
+
+    public function getDataJson()
+    {
+        return $this -> oldData;
+    }
 }
